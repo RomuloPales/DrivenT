a358a2056c031e43d795ad71cf64d89e0128a319
RomuloPales <romulopales15@gmail.com>
2023-12-14 13:51:12 -0300

    refact: new error status message

--- src/errors/canNot-List-Hotel-Error.ts
+++ src/errors/canNot-List-Hotel-Error.ts
@@ -3,6 +3,6 @@ import { ApplicationError } from '@/protocols';
 export function canNotListHotelError(): ApplicationError {
   return {
     name: 'canNotListHotelError',
-    message: 'Cannot List Hotel for this process !',
+    message: 'Cannot List Hotels!',
   };
 }
